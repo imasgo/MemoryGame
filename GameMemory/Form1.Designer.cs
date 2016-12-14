1567474156b7d17d7e8c3873efae3a5c0dbf5272
Мария Иванова <sasmashka@gmail.com>
2016-12-14 16:01:56 +0300

    All code first version

---
+++ GameMemory/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace GameMemory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Pic6Dup = new System.Windows.Forms.PictureBox();
+            this.Pic6 = new System.Windows.Forms.PictureBox();
+            this.Pic5Dup = new System.Windows.Forms.PictureBox();
+            this.Pic5 = new System.Windows.Forms.PictureBox();
+            this.Pic4Dup = new System.Windows.Forms.PictureBox();
+            this.Pic4 = new System.Windows.Forms.PictureBox();
+            this.Pic3Dup = new System.Windows.Forms.PictureBox();
+            this.Pic3 = new System.Windows.Forms.PictureBox();
+            this.Pic2Dup = new System.Windows.Forms.PictureBox();
+            this.Pic2 = new System.Windows.Forms.PictureBox();
+            this.Pic1Dup = new System.Windows.Forms.PictureBox();
+            this.Pic1 = new System.Windows.Forms.PictureBox();
+            this.ScoreLabel = new System.Windows.Forms.Label();
+            this.ScoreCount = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic6Dup)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic5Dup)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic4Dup)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic3Dup)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic2Dup)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic1Dup)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.Pic6Dup);
+            this.panel1.Controls.Add(this.Pic6);
+            this.panel1.Controls.Add(this.Pic5Dup);
+            this.panel1.Controls.Add(this.Pic5);
+            this.panel1.Controls.Add(this.Pic4Dup);
+            this.panel1.Controls.Add(this.Pic4);
+            this.panel1.Controls.Add(this.Pic3Dup);
+            this.panel1.Controls.Add(this.Pic3);
+            this.panel1.Controls.Add(this.Pic2Dup);
+            this.panel1.Controls.Add(this.Pic2);
+            this.panel1.Controls.Add(this.Pic1Dup);
+            this.panel1.Controls.Add(this.Pic1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(914, 709);
+            this.panel1.TabIndex = 0;
+            // 
+            // Pic6Dup
+            // 
+            this.Pic6Dup.Location = new System.Drawing.Point(690, 485);
+            this.Pic6Dup.Name = "Pic6Dup";
+            this.Pic6Dup.Size = new System.Drawing.Size(200, 200);
+            this.Pic6Dup.TabIndex = 11;
+            this.Pic6Dup.TabStop = false;
+            this.Pic6Dup.Tag = "6";
+            this.Pic6Dup.Click += new System.EventHandler(this.Pic6Dup_Click);
+            // 
+            // Pic6
+            // 
+            this.Pic6.Location = new System.Drawing.Point(467, 485);
+            this.Pic6.Name = "Pic6";
+            this.Pic6.Size = new System.Drawing.Size(200, 200);
+            this.Pic6.TabIndex = 10;
+            this.Pic6.TabStop = false;
+            this.Pic6.Tag = "6";
+            this.Pic6.Click += new System.EventHandler(this.Pic6_Click);
+            // 
+            // Pic5Dup
+            // 
+            this.Pic5Dup.Location = new System.Drawing.Point(243, 485);
+            this.Pic5Dup.Name = "Pic5Dup";
+            this.Pic5Dup.Size = new System.Drawing.Size(200, 200);
+            this.Pic5Dup.TabIndex = 9;
+            this.Pic5Dup.TabStop = false;
+            this.Pic5Dup.Tag = "5";
+            this.Pic5Dup.Click += new System.EventHandler(this.Pic5Dup_Click);
+            // 
+            // Pic5
+            // 
+            this.Pic5.Location = new System.Drawing.Point(19, 485);
+            this.Pic5.Name = "Pic5";
+            this.Pic5.Size = new System.Drawing.Size(200, 200);
+            this.Pic5.TabIndex = 8;
+            this.Pic5.TabStop = false;
+            this.Pic5.Tag = "5";
+            this.Pic5.Click += new System.EventHandler(this.Pic5_Click);
+            // 
+            // Pic4Dup
+            // 
+            this.Pic4Dup.Location = new System.Drawing.Point(690, 250);
+            this.Pic4Dup.Name = "Pic4Dup";
+            this.Pic4Dup.Size = new System.Drawing.Size(200, 200);
+            this.Pic4Dup.TabIndex = 7;
+            this.Pic4Dup.TabStop = false;
+            this.Pic4Dup.Tag = "4";
+            this.Pic4Dup.Click += new System.EventHandler(this.Pic4Dup_Click);
+            // 
+            // Pic4
+            // 
+            this.Pic4.Location = new System.Drawing.Point(467, 250);
+            this.Pic4.Name = "Pic4";
+            this.Pic4.Size = new System.Drawing.Size(200, 200);
+            this.Pic4.TabIndex = 6;
+            this.Pic4.TabStop = false;
+            this.Pic4.Tag = "4";
+            this.Pic4.Click += new System.EventHandler(this.Pic4_Click);
+            // 
+            // Pic3Dup
+            // 
+            this.Pic3Dup.Location = new System.Drawing.Point(243, 250);
+            this.Pic3Dup.Name = "Pic3Dup";
+            this.Pic3Dup.Size = new System.Drawing.Size(200, 200);
+            this.Pic3Dup.TabIndex = 5;
+            this.Pic3Dup.TabStop = false;
+            this.Pic3Dup.Tag = "3";
+            this.Pic3Dup.Click += new System.EventHandler(this.Pic3Dup_Click);
+            // 
+            // Pic3
+            // 
+            this.Pic3.Location = new System.Drawing.Point(19, 250);
+            this.Pic3.Name = "Pic3";
+            this.Pic3.Size = new System.Drawing.Size(200, 200);
+            this.Pic3.TabIndex = 4;
+            this.Pic3.TabStop = false;
+            this.Pic3.Tag = "3";
+            this.Pic3.Click += new System.EventHandler(this.Pic3_Click);
+            // 
+            // Pic2Dup
+            // 
+            this.Pic2Dup.Location = new System.Drawing.Point(688, 17);
+            this.Pic2Dup.Name = "Pic2Dup";
+            this.Pic2Dup.Size = new System.Drawing.Size(200, 200);
+            this.Pic2Dup.TabIndex = 3;
+            this.Pic2Dup.TabStop = false;
+            this.Pic2Dup.Tag = "2";
+            this.Pic2Dup.Click += new System.EventHandler(this.Pic2Dup_Click);
+            // 
+            // Pic2
+            // 
+            this.Pic2.Location = new System.Drawing.Point(465, 17);
+            this.Pic2.Name = "Pic2";
+            this.Pic2.Size = new System.Drawing.Size(200, 200);
+            this.Pic2.TabIndex = 2;
+            this.Pic2.TabStop = false;
+            this.Pic2.Tag = "2";
+            this.Pic2.Click += new System.EventHandler(this.Pic2_Click);
+            // 
+            // Pic1Dup
+            // 
+            this.Pic1Dup.Image = global::GameMemory.Properties.Resources.Card6;
+            this.Pic1Dup.Location = new System.Drawing.Point(241, 17);
+            this.Pic1Dup.Name = "Pic1Dup";
+            this.Pic1Dup.Size = new System.Drawing.Size(200, 200);
+            this.Pic1Dup.TabIndex = 1;
+            this.Pic1Dup.TabStop = false;
+            this.Pic1Dup.Tag = "1";
+            this.Pic1Dup.Click += new System.EventHandler(this.Pic1Dup_Click);
+            // 
+            // Pic1
+            // 
+            this.Pic1.Image = global::GameMemory.Properties.Resources.MdzRlewvieo1;
+            this.Pic1.Location = new System.Drawing.Point(19, 17);
+            this.Pic1.Name = "Pic1";
+            this.Pic1.Size = new System.Drawing.Size(200, 200);
+            this.Pic1.TabIndex = 0;
+            this.Pic1.TabStop = false;
+            this.Pic1.Tag = "1";
+            this.Pic1.Click += new System.EventHandler(this.Pic1_Click);
+            // 
+            // ScoreLabel
+            // 
+            this.ScoreLabel.AutoSize = true;
+            this.ScoreLabel.Location = new System.Drawing.Point(953, 29);
+            this.ScoreLabel.Name = "ScoreLabel";
+            this.ScoreLabel.Size = new System.Drawing.Size(55, 20);
+            this.ScoreLabel.TabIndex = 12;
+            this.ScoreLabel.Text = "Score:";
+            this.ScoreLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // ScoreCount
+            // 
+            this.ScoreCount.AutoSize = true;
+            this.ScoreCount.Location = new System.Drawing.Point(1014, 29);
+            this.ScoreCount.Name = "ScoreCount";
+            this.ScoreCount.Size = new System.Drawing.Size(18, 20);
+            this.ScoreCount.TabIndex = 13;
+            this.ScoreCount.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(981, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 20);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "5";
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 300;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1253, 719);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ScoreCount);
+            this.Controls.Add(this.ScoreLabel);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Pic6Dup)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic5Dup)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic4Dup)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic3Dup)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic2Dup)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic1Dup)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox Pic6Dup;
+        private System.Windows.Forms.PictureBox Pic6;
+        private System.Windows.Forms.PictureBox Pic5Dup;
+        private System.Windows.Forms.PictureBox Pic5;
+        private System.Windows.Forms.PictureBox Pic4Dup;
+        private System.Windows.Forms.PictureBox Pic4;
+        private System.Windows.Forms.PictureBox Pic3Dup;
+        private System.Windows.Forms.PictureBox Pic3;
+        private System.Windows.Forms.PictureBox Pic2Dup;
+        private System.Windows.Forms.PictureBox Pic2;
+        private System.Windows.Forms.PictureBox Pic1Dup;
+        private System.Windows.Forms.PictureBox Pic1;
+        private System.Windows.Forms.Label ScoreLabel;
+        private System.Windows.Forms.Label ScoreCount;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ GameMemory/Form1.cs
@@ -0,0 +1,380 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameMemory
+{
+    public partial class Form1 : Form
+    {
+        //Variables
+        int score = 0;//score
+        bool again = false;//Do you want to play again
+        Random Location = new Random();//Chooses random places for each new card
+        List<int> X = new List<int>();//list of x coordinates
+        List<int> Y = new List<int>();//list of y coordinates
+        List<int> check = new List<int>();
+        PictureBox PendingImage1;
+        PictureBox PendingImage2;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // i like u
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (PictureBox pic in panel1.Controls)
+            {
+                pic.Enabled = false;
+            }
+            timer2.Start();
+            timer1.Start();
+            Pic1.Image = Properties.Resources.Card1;
+            Pic1Dup.Image = Properties.Resources.Card1;
+            Pic2.Image = Properties.Resources.Card2;
+            Pic2Dup.Image = Properties.Resources.Card2;
+            Pic3.Image = Properties.Resources.Card3;
+            Pic3Dup.Image = Properties.Resources.Card3;
+            Pic4.Image = Properties.Resources.Card4;
+            Pic4Dup.Image = Properties.Resources.Card4;
+            Pic5.Image = Properties.Resources.Card5;
+            Pic5Dup.Image = Properties.Resources.Card5;
+            Pic6.Image = Properties.Resources.Card6;
+            Pic6Dup.Image = Properties.Resources.Card6;
+            //foreach (PictureBox pic in panel1.Controls)
+            //{
+            //    pic.Image = Properties.Resources.MdzRlewvieo1;
+            //}
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            foreach (PictureBox pic in panel1.Controls)
+            {
+                pic.Enabled = true;
+                pic.Cursor = Cursors.Hand;
+                pic.Image = Properties.Resources.MdzRlewvieo1;
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            int timer = Convert.ToInt32(label1.Text);
+            timer = timer-1;
+            label1.Text = Convert.ToString(timer);
+            if (timer == 0) { timer2.Stop(); }
+        }
+
+        #region Cards Swap
+        private void Pic1_Click(object sender, EventArgs e)
+        {
+            Pic1.Image = Properties.Resources.Card1;
+            if (PendingImage1 == null)
+            {
+                PendingImage1 = Pic1;
+            }
+            else if ((PendingImage1 != null) && (PendingImage2 == null))
+            {
+                PendingImage2 = Pic1;
+            }
+            if (PendingImage1 != null && PendingImage2 != null)
+            {
+                if (PendingImage1.Tag == PendingImage2.Tag)
+                {
+
+                }
+                else
+                {
+                    timer3.Start();
+                }
+            }
+        
+
+        }
+
+        private void Pic1Dup_Click(object sender, EventArgs e)
+        {
+            Pic1Dup.Image = Properties.Resources.Card1;
+            if (PendingImage1 == null)
+            {
+                PendingImage1 = Pic1Dup;
+            }
+            else if ((PendingImage1 != null) && (PendingImage2 == null))
+            {
+                PendingImage2 = Pic1Dup;
+            }
+            if (PendingImage1 != null && PendingImage2 != null)
+            {
+                if (PendingImage1.Tag == PendingImage2.Tag)
+                {
+
+                }
+                else
+                {
+                    timer3.Start();
+                }
+            }
+        }
+
+        private void Pic2_Click(object sender, EventArgs e)
+        {
+            Pic2.Image = Properties.Resources.Card2;
+            if (PendingImage1 == null)
+            {
+                PendingImage1 = Pic2;
+            }
+            else if ((PendingImage1 != null) && (PendingImage2 == null))
+            {
+                PendingImage2 = Pic2;
+            }
+            if (PendingImage1 != null && PendingImage2 != null)
+            {
+                if (PendingImage1.Tag == PendingImage2.Tag)
+                {
+
+                }
+                else
+                {
+                    timer3.Start();
+                }
+            }
+        }
+
+        private void Pic2Dup_Click(object sender, EventArgs e)
+        {
+            Pic2Dup.Image = Properties.Resources.Card2;
+            if (PendingImage1 == null)
+            {
+                PendingImage1 = Pic2Dup;
+            }
+            else if ((PendingImage1 != null) && (PendingImage2 == null))
+            {
+                PendingImage2 = Pic2Dup;
+            }
+            if (PendingImage1 != null && PendingImage2 != null)
+            {
+                if (PendingImage1.Tag == PendingImage2.Tag)
+                {
+
+                }
+                else
+                {
+                    timer3.Start();
+                }
+            }
+        }
+
+        private void Pic3_Click(object sender, EventArgs e)
+        {
+            Pic3.Image = Properties.Resources.Card3;
+            if (PendingImage1 == null)
+            {
+                PendingImage1 = Pic3;
+            }
+            else if ((PendingImage1 != null) && (PendingImage2 == null))
+            {
+                PendingImage2 = Pic3;
+            }
+            if (PendingImage1 != null && PendingImage2 != null)
+            {
+                if (PendingImage1.Tag == PendingImage2.Tag)
+                {
+
+                }
+                else
+                {
+                    timer3.Start();
+                }
+            }
+        }
+
+        private void Pic3Dup_Click(object sender, EventArgs e)
+        {
+            Pic3Dup.Image = Properties.Resources.Card3;
+            if (PendingImage1 == null)
+            {
+                PendingImage1 = Pic3Dup;
+            }
+            else if ((PendingImage1 != null) && (PendingImage2 == null))
+            {
+                PendingImage2 = Pic3Dup;
+            }
+            if (PendingImage1 != null && PendingImage2 != null)
+            {
+                if (PendingImage1.Tag == PendingImage2.Tag)
+                {
+
+                }
+                else
+                {
+                    timer3.Start();
+                }
+            }
+        }
+
+        private void Pic4_Click(object sender, EventArgs e)
+        {
+            Pic4.Image = Properties.Resources.Card4;
+            if (PendingImage1 == null)
+            {
+                PendingImage1 = Pic4;
+            }
+            else if ((PendingImage1 != null) && (PendingImage2 == null))
+            {
+                PendingImage2 = Pic4;
+            }
+            if (PendingImage1 != null && PendingImage2 != null)
+            {
+                if (PendingImage1.Tag == PendingImage2.Tag)
+                {
+
+                }
+                else
+                {
+                    timer3.Start();
+                }
+            }
+        }
+
+        private void Pic4Dup_Click(object sender, EventArgs e)
+        {
+            Pic4Dup.Image = Properties.Resources.Card4;
+            if (PendingImage1 == null)
+            {
+                PendingImage1 = Pic4Dup;
+            }
+            else if ((PendingImage1 != null) && (PendingImage2 == null))
+            {
+                PendingImage2 = Pic4Dup;
+            }
+            if (PendingImage1 != null && PendingImage2 != null)
+            {
+                if (PendingImage1.Tag == PendingImage2.Tag)
+                {
+
+                }
+                else
+                {
+                    timer3.Start();
+                }
+            }
+        }
+
+        private void Pic5_Click(object sender, EventArgs e)
+        {
+            Pic5.Image = Properties.Resources.Card5;
+            if (PendingImage1 == null)
+            {
+                PendingImage1 = Pic5;
+            }
+            else if ((PendingImage1 != null) && (PendingImage2 == null))
+            {
+                PendingImage2 = Pic5;
+            }
+            if (PendingImage1 != null && PendingImage2 != null)
+            {
+                if (PendingImage1.Tag == PendingImage2.Tag)
+                {
+
+                }
+                else
+                {
+                    timer3.Start();
+                }
+            }
+        }
+
+        private void Pic5Dup_Click(object sender, EventArgs e)
+        {
+            Pic5Dup.Image = Properties.Resources.Card5;
+            if (PendingImage1 == null)
+            {
+                PendingImage1 = Pic5Dup;
+            }
+            else if ((PendingImage1 != null) && (PendingImage2 == null))
+            {
+                PendingImage2 = Pic5Dup;
+            }
+            if (PendingImage1 != null && PendingImage2 != null)
+            {
+                if (PendingImage1.Tag == PendingImage2.Tag)
+                {
+
+                }
+                else
+                {
+                    timer3.Start();
+                }
+            }
+        }
+
+        private void Pic6_Click(object sender, EventArgs e)
+        {
+            Pic6.Image = Properties.Resources.Card6;
+            if (PendingImage1 == null)
+            {
+                PendingImage1 = Pic6;
+            }
+            else if ((PendingImage1 != null) && (PendingImage2 == null))
+            {
+                PendingImage2 = Pic6;
+            }
+            if (PendingImage1 != null && PendingImage2 != null)
+            {
+                if (PendingImage1.Tag == PendingImage2.Tag)
+                {
+
+                }
+                else
+                {
+                    timer3.Start();
+                }
+            }
+        }
+
+        private void Pic6Dup_Click(object sender, EventArgs e)
+        {
+            Pic6Dup.Image = Properties.Resources.Card6;
+            if (PendingImage1 == null)
+            {
+                PendingImage1 = Pic6Dup;
+            }
+            else if ((PendingImage1 != null) && (PendingImage2 == null))
+            {
+                PendingImage2 = Pic6Dup;
+            }
+            if (PendingImage1 != null && PendingImage2 != null)
+            {
+                if (PendingImage1.Tag == PendingImage2.Tag)
+                {
+
+                }
+                else
+                {
+                    timer3.Start();
+                }
+            }
+        }
+        #endregion
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            timer3.Stop();
+            PendingImage1.Image = Properties.Resources.MdzRlewvieo1;
+            PendingImage2.Image = Properties.Resources.MdzRlewvieo1;
+            PendingImage1 = null;
+            PendingImage2 = null;
+        }
+    }
+}
